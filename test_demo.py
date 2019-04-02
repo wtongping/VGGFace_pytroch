b7068f098a5a8fe92c7014301549178d41718720
wtongping <30522002+wtongping@users.noreply.github.com>
2019-04-02 11:48:01 +0800

    updata code

---
+++ test_demo.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import torch
+import cv2
+import numpy as np
+from vgg16 import VGG_16
+import torch.nn.functional as F
+
+if __name__ == "__main__":
+    model = VGG_16()
+    model.load_state_dict(torch.load("./pretrained/vgg_face.pth"))
+    model.eval()
+    im = cv2.imread("./images/Aamir_Khan1.png")
+    im = torch.Tensor(im).permute(2, 0, 1).view(1, 3, 224, 224)
+    im -= torch.Tensor(np.array([129.1863, 104.7624, 93.5940])).view(1, 3, 1, 1)
+    preds = F.softmax(model(im), -1)
+    values, index = preds.max(-1)
+    with open("./images/names.txt", 'r') as f:
+        names = f.readlines()
+    print("Index: %d, Confidence: %f, Name: %s" % (index, values, names[index]))
---
+++ vgg16.py
@@ -0,0 +1,65 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class VGG_16(nn.Module):
+    """
+    Main Class
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        super().__init__()
+        self.block_size = [2, 2, 3, 3, 3]
+        self.conv1_1 = nn.Conv2d(3, 64, 3, stride=1, padding=1)
+        self.conv1_2 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+        self.conv2_1 = nn.Conv2d(64, 128, 3, stride=1, padding=1)
+        self.conv2_2 = nn.Conv2d(128, 128, 3, stride=1, padding=1)
+        self.conv3_1 = nn.Conv2d(128, 256, 3, stride=1, padding=1)
+        self.conv3_2 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
+        self.conv3_3 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
+        self.conv4_1 = nn.Conv2d(256, 512, 3, stride=1, padding=1)
+        self.conv4_2 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv4_3 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv5_1 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv5_2 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv5_3 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.fc6 = nn.Linear(512 * 7 * 7, 4096)
+        self.fc7 = nn.Linear(4096, 4096)
+        self.fc8 = nn.Linear(4096, 2622)
+
+    def forward(self, x):
+        """ Pytorch forward
+
+        Args:
+            x: input image (224x224)
+
+        Returns: class logits
+
+        """
+        x = F.relu(self.conv1_1(x))
+        x = F.relu(self.conv1_2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2_1(x))
+        x = F.relu(self.conv2_2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv3_1(x))
+        x = F.relu(self.conv3_2(x))
+        x = F.relu(self.conv3_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv4_1(x))
+        x = F.relu(self.conv4_2(x))
+        x = F.relu(self.conv4_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv5_1(x))
+        x = F.relu(self.conv5_2(x))
+        x = F.relu(self.conv5_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc6(x))
+        # x = F.dropout(x, 0.5)
+        x = F.relu(self.fc7(x))
+        # x = F.dropout(x, 0.5)
+        return self.fc8(x)
